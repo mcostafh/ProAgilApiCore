37c4a73fe3d74da828056fcd3a287f201d0fad81
Manuel Filho <manuelferreira.costa@gmail.com>
2020-10-05 21:29:37 -0300

    Identity - DbContext - Models - Migrations

--- ProAgil.Repository/ProAgilContext.cs
+++ ProAgil.Repository/ProAgilContext.cs
@@ -1,9 +1,12 @@
-using Microsoft.EntityFrameworkCore;
 using ProAgil.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ProAgil.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProAgil.Repository
 {
-    public class ProAgilContext:DbContext
+    public class ProAgilContext :  IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int> >
     {
         public ProAgilContext( DbContextOptions<ProAgilContext> options ) : base(options) {}
         public DbSet<Evento> Eventos { get;set;}
@@ -13,7 +16,28 @@ namespace ProAgil.Repository
         public DbSet<RedeSocial> RedesSociais { get;set;}
         
         protected override void OnModelCreating(ModelBuilder ModelBuilder){
-            ModelBuilder.Entity<PalestranteEvento>()
+
+            base.OnModelCreating( ModelBuilder);
+
+            // Relacionamento N para N
+            ModelBuilder.Entity<UserRole>( userRole => {
+                userRole.HasKey( ur => new{ur.UserId, ur.RoleId});
+
+                userRole.HasOne( ur => ur.Role)
+                .WithMany( r => r.UserRoles)
+                .HasForeignKey( ur => ur.RoleId)
+                .IsRequired();
+
+                userRole.HasOne( ur => ur.User)
+                .WithMany( r => r.UserRoles)
+                .HasForeignKey( ur => ur.UserId)
+                .IsRequired();
+
+
+
+            });
+
+            ModelBuilder.Entity<PalestranteEvento>() 
             .HasKey( PE => new { PE.EventoId , PE.PalestranteId} );
 
         }
